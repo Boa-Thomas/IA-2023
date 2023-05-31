04d9f9d4cc282eb264b4e6982155c2c3dea190b7
Thomas Topfstedt <thotop100@gmail.com>
2023-05-31 16:21:53 -0300

    Update import gym.py

--- import gym.py
+++ import gym.py
@@ -25,11 +25,11 @@ class UFSCEvironment(gym.Env):
 
         # Import the grid from a csv file
         self.grid = pd.read_csv('board.csv').values.tolist()
-        print(self.grid)
+        #print(self.grid)
 
         # Get the dimensions of your grid
         self.n = len(self.grid)
-        print(self.n)
+        #print(self.n)
 
         # Define a 2D observation space
         self.observation_space = spaces.Discrete(self.n * self.n)
@@ -38,7 +38,7 @@ class UFSCEvironment(gym.Env):
     def step(self, action):
         # Check if max steps reached
         if self.current_step >= self.max_steps:
-            return self.current_state, -10, True, {}
+            return self.current_state, 0, True, {}
         self.current_step += 1
 
         # Define how the agent will step
@@ -55,12 +55,14 @@ class UFSCEvironment(gym.Env):
             x = min(x + 1, self.n - 1)
         elif action == 3:  # left
             y = max(y - 1, 0)
+        
+        # If we move out of the grid or hit the wall, stay in place and get -10 reward
+        if x < 0 or y < 0 or x >= self.n or y >= self.n or self.grid[x][y] == -50:
+            self.current_state = old_x * self.n + old_y
+            return self.current_state, -10, False, {}
 
-        # If we hit the wall, stay in place
-        if self.grid[x][y] == -50:
-            x, y = old_x, old_y
-
-        self.current_state = x * (self.n) + y  # Update current_state using 2D to 1D conversion
+        # If we didn't move out of the grid or hit a wall, update current_state
+        self.current_state = x * self.n + y  # Update current_state using 2D to 1D conversion
 
         # Calculate reward
         if type(self.grid[x][y]) is int:
@@ -91,53 +93,65 @@ class UFSCEvironment(gym.Env):
 env = UFSCEvironment()
 env.render()
 
-import numpy as np
-
 # Parameters
-alpha_values = np.arange(0, 2, 0.1)
-gamma_values = np.arange(0, 1, 0.1)
-epsilon_values = np.arange(0, 1, 0.1)
-num_episodes = 1000
+alpha = 0.6
+gamma = 0.3
+epsilon = 0.3
+num_episodes = 10000
 
-best_avg_reward = -np.inf
-best_params = None
+Q_table = np.zeros([env.observation_space.n, env.action_space.n])
+total_rewards = []
 
-for epsilon in epsilon_values:
-    for gamma in gamma_values:
-        for alpha in alpha_values:
-            Q_table = np.zeros([env.observation_space.n, env.action_space.n])
-            total_rewards = []
+for i_episode in range(num_episodes):
+    state = env.reset()
+    total_reward = 0
 
-            for i_episode in range(num_episodes):
-                state = env.reset()
-                total_reward = 0
+    for t in range(500):
+        if np.random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q_table[state])
 
-                for t in range(500):
-                    if np.random.uniform(0, 1) < epsilon:
-                        action = env.action_space.sample()
-                    else:
-                        action = np.argmax(Q_table[state])
+        next_state, reward, done, info = env.step(action)
+        old_value = Q_table[state, action]
+        next_max = np.max(Q_table[next_state])
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        Q_table[state, action] = new_value
+        state = next_state
+        total_reward += reward
 
-                    next_state, reward, done, info = env.step(action)
-                    old_value = Q_table[state, action]
-                    next_max = np.max(Q_table[next_state])
-                    new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
-                    Q_table[state, action] = new_value
-                    state = next_state
-                    total_reward += reward
+        if done:
+            print(Q_table)
+            break
 
-                    if done:
-                        break
+    total_rewards.append(total_reward)
 
-                total_rewards.append(total_reward)
+# Plot total rewards per episode
+plt.plot(total_rewards)
+plt.title('Total rewards per episode in Q-learning')
+plt.xlabel('Episode')
+plt.ylabel('Total reward')
+plt.show()
 
-            avg_reward = np.mean(total_rewards[-50:]) # average of last 50 rewards
-            print(f'Average reward: {avg_reward} with parameters: {alpha, gamma, epsilon}')
-            if avg_reward > best_avg_reward:
-                best_avg_reward = avg_reward
-                best_params = {'alpha': alpha, 'gamma': gamma, 'epsilon': epsilon}
-                print(f'Best average reward: {best_avg_reward} with parameters: {best_params}')
+# Map actions to arrow symbols
+action_map = {
+    0: '↑',  # up
+    1: '→',  # right
+    2: '↓',  # down
+    3: '←'   # left
+}
 
-print(f'Best average reward: {best_avg_reward} with parameters: {best_params}')
+# Create a grid to hold the best actions
+best_actions_grid = []
 
-# Now you can run the model again with the best parameters
+for i in range(env.n):
+    best_actions_row = []
+    for j in range(env.n):
+        state = i * env.n + j
+        best_action = np.argmax(Q_table[state])
+        best_actions_row.append(action_map[best_action])
+    best_actions_grid.append(best_actions_row)
+
+# Print the grid
+for row in best_actions_grid:
+    print(' '.join(row))
